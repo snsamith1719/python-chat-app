e6740cd339f547fd9b7cc5a02909b0f1c56755b2
snsamith1719 <samithrakshan@gmail.com>
2025-01-12 13:38:19 +0530

    first commit

---
+++ app.py
@@ -0,0 +1,59 @@
+from flask import Flask,render_template,request
+from flask_socketio import SocketIO, emit
+import random
+
+
+#Creating or Intialising Flask app
+app = Flask(__name__)
+socketio = SocketIO(app) # creating socketio instance
+
+# store connected users. Key is socket id and value is username and avatar url
+users = {}
+
+@app.route('/')
+def index():
+    return render_template('index.html')
+
+#listening fro connect event
+@socketio.on("connect")
+def handle_connect():
+    username = f"User_{random.randint(1000,9999)}"
+    gender = random.choice(["girl","boy"])
+    avatar_url = f"https://avatar.iran.liara.run/public/{gender}?username={username}"
+    users[request.sid] = {"username":username, "avatar":avatar_url}
+    emit("user_joined", {"username":username, "avatar":avatar_url},broadcast=True)
+    emit("set_username",{"username":username})
+
+
+@socketio.on("disconnect")
+def handle_disconnect():
+    user = users.pop(request.sid, None)
+    if user:
+        emit("user_left", {"username":user["username"]}, broadcast=True)
+
+
+@socketio.on("send_message")
+def handle_message(data):
+    user = users.get(request.sid)
+    if user:
+        emit("new_message", {
+            "username":user["username"],
+            "avatar":user["avatar"],
+            "message":data["message"]
+        },broadcast=True)
+
+
+@socketio.on("update_username")
+def handle_update_username(data):
+    old_username = users[request.sid]["username"]
+    new_username = data["username"]
+    users[request.sid]["username"] = new_username
+
+    emit("username_updated",{
+        "old_username":old_username,
+        "new_username":new_username
+        },broadcast=True)
+
+
+if __name__ == "__main__":
+    socketio.run(app)
